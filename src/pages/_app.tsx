b303d07bc0332b630b660aeebcd94272a5f2f139
fillip1984 <fillip1984@gmail.com>
2023-09-04 17:06:29 -0400

    initial changes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,7 +5,11 @@ import { api } from "~/utils/api";
 import "~/styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <main className="h-[100dvh] w-[100dvw] overflow-hidden bg-white text-black">
+      <Component {...pageProps} />
+    </main>
+  );
 };
 
 export default api.withTRPC(MyApp);
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,6 +3,15 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      black: "hsl(180, 40%, 14%)",
+      grey: "hsl(212, 30%, 66%)",
+      white: "hsl(240, 14%, 97%)",
+      danger: "hsl(341, 73%, 66%)",
+      warning: "hsl(55, 92%, 58%)",
+      info: "hsl(187, 62%, 66%)",
+      success: "hsl(165, 74%, 57%)",
+    },
     extend: {},
   },
   plugins: [],
